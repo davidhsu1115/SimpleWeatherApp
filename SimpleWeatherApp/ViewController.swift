9c1d570b730117b9f0ab476158b300cef6bdb1c3
Fang Wei Hsu <davidhsu1115@gmail.com>
2015-12-09 08:19:39 +0800

    Initial Commit

---
+++ SimpleWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by fangwiehsu on 2015/12/9.
+//  Copyright © 2015年 fangwiehsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
